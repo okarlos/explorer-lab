ecf1b2384d81d49a2571aa59bdb2ba279477687b
Karlos Kliemann <karloskliemann@gmail.com>
2022-10-20 06:35:19 -0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import "./css/index.css"
+
+const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
+const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
+
+function setCardType(type) {
+  const colors = {
+    visa = 
+  }
+}
